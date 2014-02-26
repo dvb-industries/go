c402bcd7c79c7a6b106c00f248ec816daa6cef78
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-26 08:17:13 +0100

    outline recipe steps

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,17 @@
-# install go
+package "mercurial"
+
+execute "mkdir tools" do
+  cwd "~/scratch/"
+  command "mkdir tools"
+  user "dvberkel"
+  group "dvberkel"
+  creates "~/scratch/tools/"
+end
+
+execute "clone go" do
+  cwd "~/scratch/tools"
+  command "hg clone"
+  user "dvberkel"
+  group "dvberkel"
+  creates "~/scratch/tools/go"
+end
